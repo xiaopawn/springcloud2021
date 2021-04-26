036cf70f7f79a51d8bbfa63205442fa317a8f576
gaozhongjun <842384656@qq.com>
2021-04-26 20:22:34 +0800

    getway与springcloud-config的学习

---
+++ cloud-config-center-3344/src/main/java/com/island/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package com.island.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author island
+ * @version 1.0 2021/4/26 19:21
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,13 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author island
+ * @version 1.0 2021/4/26 0:25
+ */
+public class T2 {
+
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+    }
+}
